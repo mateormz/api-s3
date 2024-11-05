6812de480fa2d937bdde4e1b7948aa8e6239a0c7
mateormz <165747472+mateormz@users.noreply.github.com>
2024-11-04 19:38:27 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -1,11 +1,23 @@
 import boto3
+import os
 
 def lambda_handler(event, context):
     bucket_name = event['body']['bucket_name']
     s3 = boto3.client('s3')
-    s3.create_bucket(Bucket=bucket_name)
+    
+    # Obtener la región configurada
+    region = os.getenv('AWS_REGION', 'us-east-1')
+
+    # Parámetros adicionales para evitar el error
+    if region == 'us-east-1':
+        s3.create_bucket(Bucket=bucket_name)
+    else:
+        s3.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={'LocationConstraint': region}
+        )
     
     return {
         'statusCode': 200,
-        'message': f'Bucket {bucket_name} creado exitosamente.'
+        'message': f'Bucket {bucket_name} creado exitosamente en la región {region}.'
     }
