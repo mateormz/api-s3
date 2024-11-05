038560cf6289ad4ed12e6a2b9e6f06497c81e208
mateormz <165747472+mateormz@users.noreply.github.com>
2024-11-04 19:59:42 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -2,8 +2,8 @@ import boto3
 
 def lambda_handler(event, context):
     bucket_name = event['body']['bucket_name']
-    s3 = boto3.client('s3')
-    region = 'us-east-1'
+    region = 'us-east-1'  # Cambia la región aquí si deseas otra diferente
+    s3 = boto3.client('s3', region_name=region)  # Establece la región explícitamente
 
     # Verifica si la región es us-east-1 (sin LocationConstraint)
     if region == 'us-east-1':
